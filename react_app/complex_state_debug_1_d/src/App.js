a5e0247ff5e4b8b5edebd5e29f45de997ee8fd0c
mozyao <yaomozzie@gmail.com>
2021-08-07 23:32:32 -0700

    Handling states using an array and concat

--- react_app/complex_state_debug_1_d/src/App.js
+++ react_app/complex_state_debug_1_d/src/App.js
@@ -4,51 +4,66 @@ import React,{useState} from 'react'
 
 
 const App = () => {
-  // const [left, setLeft] = useState(0)
-  // const [right, setRight] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
   //Combine left and right into a single object 
-  const[clicks, setClicks] = useState({left: 0, right: 0})
+  //const[clicks, setClicks] = useState({left: 0, right: 0})
 
-
-
-
+  // a piece of state contains an array of every clicks occurred in the app
   const [allClicks,setAll] = useState([])
-  
-  const handleLeftClick =() => {
-    const newClicks = {
-      left: clicks.left +1,
-      right: clicks.right
+  // then we can do the below event handler
+
+  const handleLeftArray = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left+1)
+
+  }
+  const handleRightArray = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
+
+  }
+
+  // const handleLeftClick =() => {
+  //   const newClicks = {
+  //     left: clicks.left +1,
+  //     right: clicks.right
        
-    }
-    setClicks(newClicks)
-  }
+  //   }
+  //   setClicks(newClicks)
+  // }
+
+
   // after 2018 we can do the following object spread syntax
-  const handlell =() => {
-    const newClicks = {
-      ...clicks,
-      left:clicks.left+1
-    }
-    setClicks(newClicks)
+  // const handlell =() => {
+  //   const newClicks = {
+  //     ...clicks,
+  //     left:clicks.left+1
+  //   }
+  //   setClicks(newClicks)
     
   
-  }
-  // we don;t need to create a new variable inside the event handlers
-  const handleRR =() =>{ 
+  // 
 
-    setClicks({...clicks, right:clicks.right+1})
+
+  // also, we don't need to create a new variable inside the event handlers
+  // const handleRR =() =>{ 
+
+  //   setClicks({...clicks, right:clicks.right+1})
   
   
-  }
-  const handleRightClick =() =>{ 
-    const newClicks = {
-      left: clicks.left ,
-      right: clicks.right + 1
+  // }
+  // const handleRightClick =() =>{ 
+  //   const newClicks = {
+  //     left: clicks.left ,
+  //     right: clicks.right + 1
     
-    }
-      setClicks(newClicks)
-  }
+  //   }
+  //     setClicks(newClicks)
+  // }
 
-  return (
+//return(
     // <div>
     //   {left}
     //   <button onClick={() => setLeft(left + 1)}>
@@ -60,19 +75,48 @@ const App = () => {
     //   {right}
     // </div>
 
+    // <div>
+    //   {clicks.left}
+    //   <button onClick={() => handlell()}>
+    //     Left
+    //   </button>
+
+      
+    //   <button onClick={() => handleRR()}>
+    //     Right
+    //   </button>
+    //   {clicks.right}
+
+    // </div>
+
+  //)
+
+
+
+
+
+
+
+  return (
+
     <div>
-      {clicks.left}
-      <button onClick={() => handlell()}>
+      {left}
+      <button onClick={handleLeftArray}>
         Left
       </button>
 
       
-      <button onClick={() => handleRR()}>
+      <button onClick={handleRightArray}>
+        
         Right
       </button>
-      {clicks.right}
+      {right}
+
+
+      <p>{allClicks.join(' ')}</p>
 
     </div>
+
 
 
 
