5dbb35a943026c7b580ca61a41cb3246ff43c7ba
mozyao <yaomozzie@gmail.com>
2021-08-08 00:10:07 -0700

    Beginning of Debug section

--- react_app/complex_state_debug_1_d/src/App.js
+++ react_app/complex_state_debug_1_d/src/App.js
@@ -2,6 +2,28 @@
 import './App.css';
 import React,{useState} from 'react'
 
+// we can use the History Component to take care of all clicks records
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 
 const App = () => {
   const [left, setLeft] = useState(0)
@@ -101,19 +123,16 @@ const App = () => {
 
     <div>
       {left}
-      <button onClick={handleLeftArray}>
-        Left
-      </button>
+      <Button handleClick={handleLeftArray} text = 'LEFT'/>
+      
 
       
-      <button onClick={handleRightArray}>
-        
-        Right
-      </button>
+      <Button handleClick={handleRightArray} text = "RIGHT" />    
       {right}
 
-
+{/* we can also build a History component to handle it*/}
       <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks}/>
 
     </div>
 
