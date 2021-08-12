170b579e7b3d57a1dc4032cf674a0db50a450b2d
mozyao <yaomozzie@gmail.com>
2021-08-12 11:01:30 -0700

    exer1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,7 +5,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const all = good+neutral+bad
+  const average = (good-bad)/all
+  const positive = good/all
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -25,9 +27,16 @@ const App = () => {
 
       <p>good {good}</p>
       
-      <p>neutral{neutral}</p>
+      <p>neutral {neutral}</p>
 
-      <p>bad{bad}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
+      
+      
+      
       
     </div>
   )
